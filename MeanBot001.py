c70a46234171c577a5daefa90e640c3e9a459989
FelixWaweru <wawerufelix60@gmail.com>
2017-09-30 00:24:57 +0300

    Upload

--- MeanBot001.py
+++ MeanBot001.py
@@ -1,15 +1,15 @@
 # import statements
 import random
 
-import tweepy,os.path, time
+import tweepy,os.path, time, secrets
 
 # Authentification
 from twitter import stream
 
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_KEY = os.environ['ACCESS_KEY']
-ACCESS_SECRET = os.environ['ACCESS_SECRET']
+CONSUMER_KEY = secrets.CONSUMER_KEY
+CONSUMER_SECRET = secrets.CONSUMER_SECRET
+ACCESS_KEY = secrets.ACCESS_KEY
+ACCESS_SECRET = secrets.ACCESS_SECRET
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
