99525bd1b1b416180f8d3e76646f6e4f89842907
FelixWaweru <wawerufelix60@gmail.com>
2018-06-08 20:05:43 +0300

    Functionality update

--- MainTerminal.py
+++ MainTerminal.py
@@ -10,12 +10,18 @@ firstOption = "0: Launch Bot "
 secondOption = "1: Launch Tweet Deleter "
 thirdOption = "2: Exit/ Terminate Bot "
 fourthOption = "3: Exit Terminal "
-print(firstOption  + "\n" + secondOption  + "\n" + thirdOption  + "\n" + fourthOption + "\n")
+
+def options():
+    print("\n")
+    print(firstOption + "\n" + secondOption + "\n" + thirdOption + "\n" + fourthOption + "\n")
 
 def terminalExecution():
     a = 1
     while a == 1:
+        options()
         selection = input("Please select value of the function you wish to execute." + "\n" + "==>")
+        #Bot launcher
+
         if selection == "0":
             print("You chose " + firstOption + "\n")
             print("Launching bot")
@@ -26,44 +32,49 @@ def terminalExecution():
             except:
                 print("Bot could not be launched. Please try again.")
 
+        #Tweet Deleter
         elif selection == "1":
             print("You chose " + secondOption + "\n")
             print("Are you sure you want to delete some tweets?" + "\n")
-            choice = input("y/n: ")
-            if choice is "y" or "Y":
+            choice1 = input("y/n: ")
+            if choice1 == "y" or choice1 == "Y":
                 try:
                     print("Tweet deletion commencing.")
                     deleteTweet = threading.Thread(target=Deleter.startDeletion())
                     deleteTweet.start()
                 except:
                     print("Tweets could not be deleted. Pls try again")
-            elif choice is "n" or "N":
+            elif choice1 == "n" or choice1 == "N":
                 print("Cancelling tweet deletion.")
 
+        #Bot Stopper
         elif selection == "2":
             print("You chose " + thirdOption + "\n")
             print("Are you sure you want to stop the bot?" + "\n")
-            choice = input("y/n: ")
-            if choice is "y" or "Y":
+            choice2 = input("y/n: ")
+            if choice2 == "y" or choice2 == "Y":
                 try:
                     ComplimentBot.exitBot()
                     print("Bot terminated successfully.")
                 except:
                     print("Bot could not be terminated. Pls try again")
-            elif choice is "n" or "N":
+            elif choice2 == "n" or choice2 == "N":
                 print("Cancelling bot termination")
 
+        #Terminal Stopper
         elif selection == "3":
             print("You chose " + fourthOption)
             print("Are you sure you want to exit the terminal?" + "\n")
-            choice = input("y/n: ")
-            if choice is "y" or "Y":
+            choice3 = input("y/n: ")
+            print(choice3)
+            if choice3 == "y" or choice3 == "Y":
                 print("Terminal ended successfully.")
                 sys.exit(0)
-            elif choice is "n" or "N":
+            elif choice3 == "n" or choice3 == "N":
                 print("Cancelling Terminal termination")
 
         else:
+            options()
             print("Please input an appropriate value.")
 
 
